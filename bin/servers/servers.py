450566260ac75665a15af54f79d9eee89ae16b6a
TheGreatStorm <thegreatstormx@gmail.com>
2021-02-24 10:07:34 -0600

    Removing duplicate code.

--- bin/servers/servers.py
+++ bin/servers/servers.py
@@ -13,7 +13,7 @@ def command_prefix(container, command, user):
 
 
 class RustServer:
-    def __init__(self, image=None, container=None, config_json=None, app_dir=None, config_file=None):
+    def __init__(self, image=None, container=None, config_json=None, app_dir=None, config_file=None, game_server=None):
         self.image = image
         self.container = container
         self.config_json = config_json
@@ -43,6 +43,11 @@ class RustServer:
             self.config_file = ""
         else:
             self.config_file = config_file
+
+        if game_server is None:
+            self.game_server = ""
+        else:
+            self.game_server = game_server
 
     def install(self):
         data = {}
@@ -69,14 +74,16 @@ class RustServer:
             commands.append("echo 'export app_port={}' >> /etc/bashrc".format(app_port))
             commands.append("echo 'echo -e \'Welcome to Storm Pods! Server Port: {} Rcon Port: {} Mobile Port: {} \'' >> /etc/bashrc".format(game_port,rcon_port,app_port))
             commands.append('git clone https://github.com/thegreatstorm/ansiblepods.git /opt/ansiblepods > /dev/null')
-            commands.append('ansible-playbook /opt/ansiblepods/linuxgsm/rustserver/requirements.yml')
-            commands.append('ansible-playbook /opt/ansiblepods/linuxgsm/rustserver/setup.yml')
-            commands.append('ansible-playbook /opt/ansiblepods/linuxgsm/rustserver/install.yml')
+            commands.append('ansible-playbook /opt/ansiblepods/linuxgsm/requirements/{0}.yml --extra-vars \'game_server={0}\''.format(self.game_server))
+            commands.append('ansible-playbook /opt/ansiblepods/linuxgsm/setup.yml --extra-vars \'game_server={}\''.format(self.game_server))
+            commands.append('ansible-playbook /opt/ansiblepods/linuxgsm/install.yml --extra-vars \'game_server={}\''.format(self.game_server))
             commands.append('chmod -R 777 /opt')
 
             for command in commands:
                 command = command_prefix(data["container_id"], command, 'root')
                 os.system(command)
+
+            print("To access container: docker exec -u {} -it {} /bin/bash".format(self.game_server, self.container))
 
         except Exception as e:
             print("Failed to create container: {}".format(str(e)))
@@ -86,88 +93,10 @@ class RustServer:
 
     def start(self):
         # Currently disabled until environment variables figured out.
-        command = "docker cp {} {}:/home/rustserver/lgsm/config-lgsm/rustserver/rustserver.cfg".format(self.config_file, self.container)
+        command = "docker cp {0} {1}:/home/{2}/lgsm/config-lgsm/rustserver/rustserver.cfg".format(self.config_file, self.container, self.game_server)
         os.system(command)
         command = "ansible-playbook /opt/ansiblepods/linuxgsm/rustserver/start.yml"
         command = command_prefix(self.container, command, 'rustserver')
         print(command)
         os.system(command)
 
-
-class Valheim:
-    def __init__(self, image=None, container=None, config_json=None, app_dir=None, config_file=None):
-        self.image = image
-        self.container = container
-        self.config_json = config_json
-        self.app_dir = app_dir
-
-        if image is None:
-            self.image = ""
-        else:
-            self.image = image
-
-        if config_json is None:
-            self.config_json = ""
-        else:
-            self.config_json = config_json
-
-        if container is None:
-            self.container = ""
-        else:
-            self.container = container
-
-        if app_dir is None:
-            self.app_dir = ""
-        else:
-            self.app_dir = app_dir
-
-        if config_file is None:
-            self.config_file = ""
-        else:
-            self.config_file = config_file
-
-    def install(self):
-        data = {}
-        game_port = random_port()
-        query_port = random_port()
-
-        command = "docker run -td -p {0}:{0}/udp -p {0}:{0}/tcp -p {1}:{1}/tcp -p {1}:{1}/udp {2}".format(game_port, query_port, self.image)
-
-        try:
-            container_id = check_output(command, shell=True).decode('ascii')
-            container_id = container_id.rstrip("\n")
-
-            data["container_id"] = container_id
-            data["game_port"] = game_port
-            data["query_port"] = query_port
-
-            print(str(data))
-            # Insert New Record into database.
-            commands = []
-            commands.append("echo 'export server_port={}' >> /etc/bashrc".format(game_port))
-            commands.append("echo 'export query_port={}' >> /etc/bashrc".format(query_port))
-            commands.append("echo 'echo -e \'Welcome to Storm Pods! Server Port: {} Query Port: {} \'' >> /etc/bashrc".format(game_port, query_port))
-            commands.append('git clone https://github.com/thegreatstorm/ansiblepods.git /opt/ansiblepods > /dev/null')
-            commands.append('ansible-playbook /opt/ansiblepods/linuxgsm/valheim/requirements.yml')
-            commands.append('ansible-playbook /opt/ansiblepods/linuxgsm/valheim/setup.yml')
-            commands.append('ansible-playbook /opt/ansiblepods/linuxgsm/valheim/install.yml')
-            commands.append('chmod -R 777 /opt')
-
-            for command in commands:
-                command = command_prefix(data["container_id"], command, 'root')
-                os.system(command)
-
-        except Exception as e:
-            print("Failed to create container: {}".format(str(e)))
-            data["status"] = "Failed to create container. {}".format(str(e))
-
-        return data
-
-    def start(self):
-        # Currently disabled until environment variables figured out.
-        command = "docker cp {} {}:/home/rustserver/lgsm/config-lgsm/valheim/rustserver.cfg".format(self.config_file, self.container)
-        os.system(command)
-        command = "ansible-playbook /opt/ansiblepods/linuxgsm/valheim/start.yml"
-        command = command_prefix(self.container, command, 'rustserver')
-        print(command)
-        os.system(command)
